d6c2bfe7378abfdc85acc7551722ed0a2009397a
hqthai <99537336+hqthai@users.noreply.github.com>
2024-07-29 21:56:33 +0700

    update: list user

--- TienDaoAPI/Services/IServices/IUserService.cs
+++ TienDaoAPI/Services/IServices/IUserService.cs
@@ -4,9 +4,7 @@ namespace TienDaoAPI.Services.IServices
 {
     public interface IUserService
     {
-        public Task<User> CreateNewUser(User user);
-        public Task<User?> FindByEmailAsync(string email);
-        public bool CheckPassword(User user, string password);
         public Task<User?> GetUserByIdAsync(int id);
+        public Task<IEnumerable<User?>> GetAllUsers();
     }
 }
